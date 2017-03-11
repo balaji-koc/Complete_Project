3f011197ba0c8828078ac4d3dfe54699a3e2e22d
Balaji <balaji.koc@gmail.com>
2017-03-11 16:24:42 +0530

    First Commit

---
+++ com.completeproject/src/main/java/com/completeproject/AppUnderTest.java
@@ -0,0 +1,10 @@
+package com.completeproject;
+
+public class AppUnderTest {
+	
+	public void appMethod()
+	{
+		System.out.println("*** In AUT class App Method ***");
+	}
+
+}
---
+++ com.completeproject/src/test/java/TestDriver.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+import com.completeproject.AppUnderTest;
+
+public class TestDriver {
+  @Test
+  public void driverMethod() {
+	  
+	  AppUnderTest aut = new AppUnderTest();
+	  aut.appMethod();
+	  
+	  System.out.println("@@@@@ In the Test Class Method @@@@ ");
+	  
+  }
+}
